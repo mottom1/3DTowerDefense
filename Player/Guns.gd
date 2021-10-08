72f4f8d52da455833da5c93b7de5963e6a6d42a9
Thomas <87886488+mottom1@users.noreply.github.com>
2021-10-08 12:01:44 -0700

    Update Guns.gd
    This Has some animations too

--- Player/Guns.gd
+++ Player/Guns.gd
@@ -2,24 +2,45 @@ extends Spatial
 var damadge = 10
 onready var ARAnimation = $Hand/AR/AnimationPlayer
 onready var Raycast = $LookAtTheShote
-
+var Bullets := 12
+var reloadbullets := 12
 var curentgun : String = "AR"
-	
-func FireAR():
+var allreadystarted :bool = false
 
-	if not ARAnimation.is_playing():
+
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("Shoot") and !allreadystarted:
+		if Bullets >0:
+			FireAR()
+		else:
+			allreadystarted = true
+			ARAnimation.play("Reload")
+			print(str(Bullets))
+			yield(ARAnimation, "animation_finished")
+			Bullets = reloadbullets
+			
+			allreadystarted = false
+		
+	if Input.is_action_just_released("Shoot") and !allreadystarted:
+		ARAnimation.stop()
+
+func FireAR():
+	
+		if not ARAnimation.is_playing():
+			Bullets -= 1
 			if Raycast.is_colliding():
 				var target = Raycast.get_collider() 
 				if target.is_in_group("Enimes"):
 					if target.is_in_group("Enimes"):
 						target.Health -= damadge
 						print(str(target.Health))
-	ARAnimation.play("Shoot")
+		ARAnimation.play("Shoot")
 	
 	
 
-func _physics_process(delta):
-	if Input.is_action_pressed("Shoot"):
-			FireAR()
-	if Input.is_action_just_released("Shoot"):
-		ARAnimation.stop()
+
+	
+	
+	
