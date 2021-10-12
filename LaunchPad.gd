50280efb73881635d5c27a34bb931d1d14970dbc
Thomas <87886488+mottom1@users.noreply.github.com>
2021-10-12 12:20:42 -0700

    Got TO get this done

--- LaunchPad.gd
+++ LaunchPad.gd
@@ -1,16 +1,30 @@
 extends Area
+var exlarate = false
+export var maxspeed = 50
+export var speedup = 15
+export var direction = Vector3(0,1,0)
+var curentspeed = Vector3()
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+	
+
+func LaunchPlayer():
+	if exlarate:
+		curentspeed += speedup*direction
+		GlobalVars.playervars.EnviermentalMovment += curentspeed
+		if abs(curentspeed.x) >=maxspeed:
+			exlarate = false
+			curentspeed = Vector3.ZERO
+		if abs(curentspeed.z) >=maxspeed:
+			exlarate = false
+			curentspeed = Vector3.ZERO
+		if abs(curentspeed.y) >=maxspeed:
+			exlarate = false
+			curentspeed = Vector3.ZERO
+		
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_LaunchPad_body_entered(body):
+	LaunchPlayer()
+	var launchy = body
+	print(str(launchy))
